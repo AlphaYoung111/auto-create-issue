707cb5ac6a2907bd31a87960dd43cbb6e9e0485f
AlphaYoung111 <729346015@qq.com>
2021-12-15 20:47:13 +0800

    all-done

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { Octokit } = require('octokit')
 
 const core = require('@actions/core')
 
+const dayjs = require('dayjs')
+
 const token = core.getInput('token-prop')
+
 
 const octokit = new Octokit({
   auth: token
@@ -10,9 +13,13 @@ const octokit = new Octokit({
 
 
 
+const getTitle = () => dayjs().format('YYYY-MM-DD')
+
+const getBody = () => `今天是${dayjs().get('date')}，早上好！`
+
 octokit.rest.issues.create({
   owner: "AlphaYoung111",
   repo: "auto-create-issue",
-  title: "Hello world from issue",
-  body: 'test'
+  title: getTitle(),
+  body: getBody()
 });
